eb9d41c020fb3f1a65d99d04c9e842ef56bdc859
Miquel Moli Gonzalez <miquel.moligonzalez@imean-biotech.com>
2025-11-08 13:47:57 +0100

    add volcanoplot with significant gene labels if selected option

--- server.R
+++ server.R
@@ -52,6 +52,16 @@ server <- function(input, output) {
     yvals <- -log10(data$padj)
     sig_up <- data$log2FC >= input$log2FCslider & data$padj <= 10^(-input$pvalueslider)
     sig_down <- data$log2FC <= -input$log2FCslider & data$padj <= 10^(-input$pvalueslider)
+    
+    # Récupérer les labels des gènes significatifs pour l'affichage du volcano plot
+    
+    significant_labels <- if (input$display_significant_labels) {
+      ifelse(sig_up | sig_down, data$GeneName, "")
+    } else {
+      rep("", nrow(data))
+    }
+    
+    # Définir les couleurs des gènes significatifs
     colors <- rep("grey", nrow(data))
     colors[sig_up] <- "violet"
     colors[sig_down] <- "pink"
@@ -60,9 +70,11 @@ server <- function(input, output) {
       x = ~data$log2FC,
       y = ~yvals,
       type = "scatter",
-      mode = "markers",
+      mode = "markers+text",
       marker = list(color = if (input$color_by_significant_genes) colors else "grey", size = 10),
-      text = if (input$display_significant_labels) ~data$GeneName,
+      text = significant_labels,
+      hovertext = data$GeneName,
+      textposition = "top center",
       hoverinfo = "text+x+y"
     ) %>%
       layout(
@@ -70,15 +82,28 @@ server <- function(input, output) {
         xaxis = list(title = "log2(Fold Change)"),
         yaxis = list(title = "-log10(padj)"),
         shapes = if (input$display_threshold_lines) {
+          
           list(
             # Lignes verticales
-            list(type = "line", x0 = -input$log2FCslider, x1 = -input$log2FCslider,
-                 y0 = 0, y1 = max(yvals, na.rm = TRUE), line = list(dash = "dash", color = "purple")),
-            list(type = "line", x0 = input$log2FCslider, x1 = input$log2FCslider,
-                 y0 = 0, y1 = max(yvals, na.rm = TRUE), line = list(dash = "dash", color = "purple")),
+            list(type = "line", 
+                 x0 = -input$log2FCslider, 
+                 x1 = -input$log2FCslider,
+                 y0 = 0, y1 = max(yvals, na.rm = TRUE), 
+                 line = list(dash = "dash", color = "purple")),
+            
+            list(type = "line", 
+                 x0 = input$log2FCslider, 
+                 x1 = input$log2FCslider,
+                 y0 = 0, 
+                 y1 = max(yvals, na.rm = TRUE), 
+                 line = list(dash = "dash", color = "purple")),
+            
             # Ligne horizontale
-            list(type = "line", x0 = min(data$log2FC, na.rm = TRUE), x1 = max(data$log2FC, na.rm = TRUE),
-                 y0 = input$pvalueslider, y1 = input$pvalueslider,
+            list(type = "line", 
+                 x0 = min(data$log2FC, na.rm = TRUE), 
+                 x1 = max(data$log2FC, na.rm = TRUE),
+                 y0 = input$pvalueslider, 
+                 y1 = input$pvalueslider,
                  line = list(dash = "dash", color = "purple"))
           )
         }
